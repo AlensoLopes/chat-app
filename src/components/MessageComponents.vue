93ad1068b6ea6ec98cf5c53b9f4c8a1346aef924
Alenso Lopes <alenso.lopes@gmail.com>
2024-02-07 14:06:03 +0100

    feat: delete

--- src/components/MessageComponents.vue
+++ src/components/MessageComponents.vue
@@ -1,5 +1,6 @@
 <script setup>
 import { computed } from 'vue';
+import { TrashIcon } from '@heroicons/vue/24/solid';
 const props = defineProps({
   message: {
     type: Object,
@@ -7,9 +8,11 @@ const props = defineProps({
   }
 });
 
+const emit = defineEmits(["delete"])
+
 const formatDate = (date) => {
-  let formattedDate = date.toLocaleDateString();
-  let formattedTime = date.toLocaleTimeString('default', '2-digit', '2-digit');
+  const formattedDate = date.toLocaleDateString();
+  const formattedTime = date.toLocaleTimeString('default', '2-digit', '2-digit');
   if(formattedDate === new Date().toLocaleDateString())
     return `Aujourd'hui à ${formattedTime}`;
   return `${formattedDate} à ${formattedTime}`;
@@ -22,16 +25,19 @@ const formattedDate = computed(() => {
 </script>
 
 <template>
-  <div class="flex flex-row items-center w-full">
-    <div class='flex justify-start items-center'>
+  <div class="flex flex-row items-center w-full group">
+    <div class='flex justify-start items-center w-full'>
       <img :src='message.user.avatarUrl' :alt='message.user.name' class="h-16 rounded-full">
       <div class='flex flex-col p-2'>
         <h2 class="text-lg">{{ message.user.name }}</h2>
         <p class="text-xs text-gray-500">{{ formattedDate }}</p>
       </div>
+      <div class='flex justify-start p-4 gap-4 w-full'>
+        <p class="text-lg p-2 w-full">{{ message.message }}</p>
+      </div>
     </div>
-    <div class='flex justify-center p-4'>
-      <p class="text-lg p-2">{{ message.message }}</p>
+    <div class='flex justify-end w-full p-4 gap-4'>
+      <button @click="emit('delete', message.id)" class="group-focus group-hover:block p-2 hidden"><TrashIcon class="h-6 w-6"/></button>
     </div>
   </div>
 </template>
--- src/views/ChatView.vue
+++ src/views/ChatView.vue
@@ -3,32 +3,41 @@ import MessageComponents from '@/components/MessageComponents.vue';
 import { ref } from "vue";
 const message = ref('');
 const messageList = ref([]);
+const messageInput = ref(null)
 
 const addMessage = () => {
   if(message.value === '') 
     return;
   messageList.value.push({
+    id: Math.random().toString(32).slice(2),
     message: message.value,
     date: new Date(),
     user: {
-      name: 'Eric Porcq',
+      name: 'Jean-Luc Lambert',
       avatarUrl: 
-      'https://media.licdn.com/dms/image/C4D03AQH0TuKfHrOpfQ/profile-displayphoto-shrink_400_400/0/1607679351861?e=2147483647&v=beta&t=a2zxWJKCOT4EasStDlHtACsP2DwoH5fFWX6uudlQpe4'
+      'https://media.licdn.com/dms/image/C4D03AQE30YtF8pwv-Q/profile-displayphoto-shrink_800_800/0/1605694187785?e=2147483647&v=beta&t=MRnxl_KBJ17-TcmyfPmWRNIecBPXQkl9KHB9h_goHOs'
     }
   });
   message.value = '';
   console.log(messageList.value);
+
+  messageInput.value.focus()
 };
+
+const deleteMessage = (id) => messageList.value = messageList.value.filter(message => message.id !== id)
+
 </script>
 
 <template>
   <div class='flex flex-col w-full justify-center'>
     <div v-for="(message, index) in messageList" :key="index" class="border rounded-md p-2 m-2 bg-gray-800 flex flex-col-reverse">
-      <MessageComponents :message="message" />
+      <MessageComponents @delete="deleteMessage" :message="message" />
     </div>
     <div class='flex flex-row w-full justify-center p-2 fixed bottom-4'>
-      <textarea v-model="message" name='message' id='message' rows='' class="text-black rounded-md"></textarea>
-      <button @click="addMessage" class="ml-2 p-2 rounded-md bg-blue-600">Send</button>
+      <textarea ref="messageInput"
+      @keyup.enter.exact="addMessage"
+      v-model="message" name='message' id='message' rows='1' class="text-black rounded-md w-full"></textarea>
+      <button @delete="deleteMessage" @click="addMessage" class="ml-2 p-4 rounded-md bg-blue-600">Send</button>
     </div>
   </div>
 </template>
